a25bad0e64fe4798d9ce92ffb7afaa211168cdf1
Diane Douglas <diane.douglas1@gmail.com>
2015-09-28 02:45:02 -0700

    Add sign out button with signOut method, also in students controller.

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -4,10 +4,14 @@ attendanceApp.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsF
     //now you can call methods that use shared data that are declared in the factory from your HTML 
     $scope.StudentsFactory = StudentsFactory; 
 
-    //signIn method is in the students controller since the directions say that students want to sign in
+    //signIn and signOut methods are here in the students controller since the directions say that students want to sign in/out
     //teachers just want to view which students are signed in and which aren't. They don't need to sign students in.
     //if they both needed to sign students in and out then the method would be in the factory.
     $scope.signIn = function(student) {
         student.present = true;
+    };
+    
+    $scope.signOut = function(student) {
+        student.present = false;
     };
 });
